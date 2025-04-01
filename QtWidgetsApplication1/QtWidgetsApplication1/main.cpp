ade83539253cd5c731b683875c0eed30c57b1921
JoMeka1 <mekalafontaine@gmail.com>
2025-04-01 14:08:04 -0400

    mettre fenetre principal et photo background meilleur score

--- QtWidgetsApplication1/QtWidgetsApplication1/main.cpp
+++ QtWidgetsApplication1/QtWidgetsApplication1/main.cpp
@@ -9,6 +9,7 @@
 #include "Gameplay.h"
 #include <QThread>
 #include <QDebug>
+#include <QMainWindow>
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
@@ -16,41 +17,35 @@ int main(int argc, char* argv[]) {
     const ComMode MODE = CLAVIER;
     const std::string NOM_PORT = "COM3";
 
-    QStackedWidget stack;
+    // Création de la fenêtre principale
+    QMainWindow mainWindow;
+    mainWindow.setWindowTitle("Guitar Hero UDS");
+
+    // Création du gestionnaire de pages
+    QStackedWidget* stack = new QStackedWidget();
+
+    // Initialisation du jeu
     Gameplay* gameplay = new Gameplay(NOM_PORT, MODE);
 
-    // Ajouter les fenêtres
-    fenetres QtFenetre = Accueil;
-    myQtManager::qtPageMenu(nullptr, &stack, gameplay);
-    myQtManager::qtPageAccueil(nullptr, &stack, gameplay);
-    myQtManager::qtPageMeilleurScore(nullptr, &stack, gameplay);
-    myQtManager::qtPageInformations(nullptr, &stack, gameplay);
-    myQtManager::qtPageParametres(nullptr, &stack, gameplay);
-    myQtManager::qtPageGame(nullptr, &stack, gameplay);
+    // Ajout des pages au QStackedWidget
+    myQtManager::qtPageMenu(nullptr, stack, gameplay);
+    myQtManager::qtPageAccueil(nullptr, stack, gameplay);
+    myQtManager::qtPageMeilleurScore(nullptr, stack, gameplay);
+    myQtManager::qtPageInformations(nullptr, stack, gameplay);
+    myQtManager::qtPageParametres(nullptr, stack, gameplay);
+    myQtManager::qtPageGame(nullptr, stack, gameplay);
 
-    stack.setCurrentIndex(QtFenetre);
-    stack.resize(TAILLE_ECRAN_X, TAILLE_ECRAN_Y);
-    stack.show();
+    // Définition de la fenêtre principale avec le stack
+    mainWindow.setCentralWidget(stack);
 
-    /*int entier = 0;
-    qDebug() << "debug entier actuel: " << entier;
+    // Mode plein écran
+    mainWindow.showFullScreen();
 
-    while (entier < 10) {
-        // Exécuter la logique de votre jeu ici
-        entier++;
-        qDebug() << "debug entier actuel: " << entier;
-        cout << "cout entier actuel: " << entier;
-
-        // Traiter les événements Qt pour permettre les rafraîchissements
-        QApplication::processEvents();
-
-        // Pour éviter une boucle infinie sans délai
-        QThread::msleep(10);
-    }*/
+    // Définir la page d'accueil comme première page affichée
+    stack->setCurrentIndex(Accueil);
 
     return app.exec();
 }
-
 /*int main(int argc, char* argv[]) {    //main projet 1
 
     const ComMode MODE = CLAVIER;
