76240fb59f1194142751ada2f9ee1142239566ee
rems <rems@gmail.com>
2025-04-05 12:12:19 -0400

    feat: fonctionne avec QTimer et recoit action ardinateur

--- QtWidgetsApplication1/QtWidgetsApplication1/Note.h
+++ QtWidgetsApplication1/QtWidgetsApplication1/Note.h
@@ -17,16 +17,16 @@ struct Note {
         : tempsAffichage(tAffichage), tempsDepart(tDepart), duree(tdurree), couleur(couleurBouton) {
     }
 
-    // Surcharge de l'opérateur d'affectation
-    Note& operator=(const Note& other) {
-        if (this != &other) { // Vérification d'auto-affectation
-            tempsAffichage = other.tempsAffichage;
-            tempsDepart = other.tempsDepart;
-            duree = other.duree;
-            couleur = other.couleur;
-            etat = other.etat;
-            action = other.action;
-        }
-        return *this;
-    }
+    //// Surcharge de l'opérateur d'affectation
+    //Note& operator=(const Note& other) {
+    //    if (this != &other) { // Vérification d'auto-affectation
+    //        tempsAffichage = other.tempsAffichage;
+    //        tempsDepart = other.tempsDepart;
+    //        duree = other.duree;
+    //        couleur = other.couleur;
+    //        etat = other.etat;
+    //        action = other.action;
+    //    }
+    //    return *this;
+    //}
 };
