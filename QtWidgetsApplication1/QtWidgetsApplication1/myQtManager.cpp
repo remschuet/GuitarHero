28fe2a5a08179ab32c2f7ec019705f49fb88b1e9
JoMeka1 <mekalafontaine@gmail.com>
2025-04-05 13:33:30 -0400

    commentaires enleve

--- QtWidgetsApplication1/QtWidgetsApplication1/myQtManager.cpp
+++ QtWidgetsApplication1/QtWidgetsApplication1/myQtManager.cpp
@@ -1137,10 +1137,8 @@ void myQtManager::qtPageGame(QWidget* window, QStackedWidget* stack, Gameplay* G
     QLabel* ProgressionLabel = new QLabel(pageGame);
 
     int pageIndex = stack->addWidget(pageGame); // Add the page and get its index
-    qDebug() << "PageGame ajouté à l'index :" << pageIndex;
 
     QObject::connect(stack, &QStackedWidget::currentChanged, [stack, pageGame, G, gameLabel, titleLabel, ProgressionLabel, manager, layoutGame](int index) {
-        qDebug() << "Index actuel changé à :" << index;
         if (stack->widget(index) == pageGame) {
             qDebug() << "PageGame est affichée!";
             G->gameStruct.chansonEnCours = new Chanson(CHANSON_2_MP3);
